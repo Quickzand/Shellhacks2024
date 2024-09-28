45b7e2d64f5e8c0d795131e70f04b6106ab2056c
Matthew Sand <44583344+Quickzand@users.noreply.github.com>
2024-09-27 23:30:13 -0400

    Initial Commit

---
+++ Itemize/Itemize/ItemizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemizeApp.swift
+//  Itemize
+//
+//  Created by Matthew Sand on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ItemizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
