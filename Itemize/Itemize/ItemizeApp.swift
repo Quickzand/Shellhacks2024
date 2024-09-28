b5d0ef5d07d8216f9edabb880a8bc2f11b97bb29
Matthew Sand <44583344+Quickzand@users.noreply.github.com>
2024-09-28 00:48:09 -0400

    Swapped back to swift

---
+++ Itemize/Itemize/ItemizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemizeApp.swift
+//  Itemize
+//
+//  Created by Matthew Sand on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ItemizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
